8f40d982f18fede1b33dc678b7f74edbcdc7eb78
Lukas Dahlberg <Sarlacc83@gmail.com>
2020-04-24 11:42:37 -0700

    url-file-processor Initial commit

---
+++ src/main/java/com/ebayexample/urlfileprocessor/UrlFileProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.ebayexample.urlfileprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlFileProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlFileProcessorApplication.class, args);
+	}
+
+}
